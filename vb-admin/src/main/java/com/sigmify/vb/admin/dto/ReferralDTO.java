82f60ca8526e8d00d6c427109075eca43b32fc08
Asu <you@example.com>
2021-04-17 18:25:20 -0700

    referralDTo

---
+++ vb-admin/src/main/java/com/sigmify/vb/admin/dto/ReferralDTO.java
@@ -0,0 +1,12 @@
+package com.sigmify.vb.admin.dto;
+
+import java.io.Serializable;
+
+public class ReferralDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 742585157638870238L;
+
+}
